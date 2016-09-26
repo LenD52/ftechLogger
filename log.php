407e6fff3b11b00f113faf5de3da182fd02c5b5a
LenD52 <maximka-cosmo@yandex.ru>
2016-09-26 16:09:28 +0300

    Update log.php

--- log.php
+++ log.php
@@ -7,24 +7,20 @@
         private $dateLog; // Время/Дата лога.
 		private $countogs = 0; // Количество строк логов.
 
-        public function __construct($text)
-		{
+        public function __construct($text) {
             $this->textLog = $text;
             $this->dateLog = date('Y-m-d G:i:s');
         }
 
-        public function showText()
-		{
+        public function showText() {
             return $this->textLog;
         }
         
-        public function showDate()
-		{
+        public function showDate() {
             return $this->dateLog;
         }
         
-        public function adLog($tableName)
-		{            
+        public function adLog($tableName) {            
             if(mysql_query("INSERT INTO ".$tableName." (Text,Date) VALUES('".$this->textLog."','".$this->dateLog."')")){            
                 return 'Сохранение прошло успешно.<br>';
             } else {
@@ -32,8 +28,7 @@
             }
         }
         
-        public function checkLogs($tableName)
-		{
+        public function checkLogs($tableName) {
             $result = mysql_query("SELECT * FROM ".$tableName);
             while ($row = mysql_fetch_array($result, MYSQL_ASSOC)){
                 $this->logs[$this->countogs] = "[".$row['Date']."] ".$row['Text']."<br>";
@@ -43,8 +38,7 @@
             mysql_free_result($result);
         }
         
-        public function showLogs()
-		{    
+        public function showLogs(){    
             for ($i = 0; $i <= count($this->logs); $i++) {
                 echo $this->logs[$i];
             }
